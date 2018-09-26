7b4e85418b027fdb9e19d7c05e361b6c4a2fa193
jainsarthak277 <saja3209@colorado.edu>
2018-09-25 20:08:38 -0600

    Deadline for 9/25 met

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-objects = main.o
-header = main.h
+objects = main.o help.o
+header = main.h help.h
 
 memfun : $(objects) $(headers)
 	gcc -o memfun $(objects)
--- help.c
+++ help.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include "help.h"
 
-void help(void)
+void help_fn(char *dummy_h)
 {
- printf("You are in help function.\n");
+ printf("\nYou are in help function.\n");
+ printf("These are some common Memory Fun commands:\n\n");
+ printf("allocate	Allocate a block of memory\n");
+ printf("free		Release previously allocated block\n");
+ printf("display		Display contents of memory\n");
+ printf("write		Write at indicated memory location\n");
+ printf("invert		Invert all memory bits\n\n");
+ printf("Functionality of above in progress\n");
  printf("Sorry to not be of any more help.\n");
 }
-
