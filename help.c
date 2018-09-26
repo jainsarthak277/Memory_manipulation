f245747279f6cfc898a599d602852597e6dc3aee
jainsarthak277 <saja3209@colorado.edu>
2018-09-25 23:21:24 -0600

    final sprint 1

--- help.c
+++ help.c
@@ -5,11 +5,11 @@ void help_fn(char *dummy_h)
 {
  printf("\nYou are in help function.\n");
  printf("These are some common Memory Fun commands:\n\n");
- printf("allocate	Allocate a block of memory\n");
- printf("free		Release previously allocated block\n");
- printf("display		Display contents of memory\n");
- printf("write		Write at indicated memory location\n");
- printf("invert		Invert all memory bits\n\n");
+ printf("allocate\tAllocate a block of memory\n");
+ printf("free\t\tRelease previously allocated block\n");
+ printf("display\t\tDisplay contents of memory\n");
+ printf("write\t\tWrite at indicated memory location\n");
+ printf("invert\t\tInvert all memory bits\n\n");
  printf("Functionality of above in progress\n");
  printf("Sorry to not be of any more help.\n");
 }
