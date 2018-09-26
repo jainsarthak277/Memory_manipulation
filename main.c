f1ca030f0a2a257360bfc1d6e7823fabd8686127
VatsalSheth <vash3665@colorado.edu>
2018-09-25 18:55:18 -0600

    Main bug fixed

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ char prompt[10];
 __uint32_t e;
 __uint32_t com_key;
 
-static decode lookup[] = { {"q",close}, {"Q",close}, {"help",help} }; 
+static decode lookup[] = { {"q\n",close}, {"Q\n",close}, {"help\n",help} }; 
 #define N (sizeof(lookup))/(sizeof(decode))
 
 void main()
@@ -14,7 +14,6 @@ void main()
 		if(*fgets(prompt,10,stdin) != '\n')
 		{
 			com_key = find_command(prompt);	
-			printf("%d\n",com_key);		
 			if(com_key!=0)	
 			{
 				switch(com_key)
@@ -30,10 +29,8 @@ void main()
 
 __uint32_t find_command(char *command)
 {
-	//printf("%s   %s",command,lookup[0].com);
 	for(__uint32_t i=0; i<N; i++)
 	{
-		//decode *tmp = (lookup + i*sizeof(decode));
 		if(strcmp(lookup[i].com,command)==0)
 		{
 			return lookup[i].val;
