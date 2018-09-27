8a25ad13d101565edde8808a79b2d01bdc155882
VatsalSheth <vash3665@colorado.edu>
2018-09-27 00:25:13 -0600

    Main backbone ready git add main.h

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-char prompt[10];
+char *prompt;
 char *command, *option;
 __uint32_t e;
 
-static decode lookup[] = { {"q\n",&quit}, {"Q\n",&quit}, {"help\n",&help_fn} }; 
+static decode lookup[] = { {"q",&quit}, {"Q",&quit}, {"help",&help_fn} }; 
 #define N (sizeof(lookup))/(sizeof(decode))
 
 void main()
 {
+	prompt = (char*)malloc(20);	
 	command = (char*)malloc(10);	
 	option = (char*)malloc(10);
 	e=0;
@@ -17,18 +18,21 @@ void main()
 	{
 		if(*fgets(prompt,10,stdin) != '\n')
 		{
-			call_command(prompt);	
+			prompt=remove_spaces(prompt);
+			command = strtok(prompt,"-");	
+			option = strtok(NULL,"\n");		
+			call_command(command,option);	
 		}
 	}
 }
 
-void call_command(char *command)
+void call_command(char *command, char *option)
 {
 	for(__uint32_t i=0; i<N; i++)
 	{
 		if(strcmp(lookup[i].com,command)==0)
 		{
-			(*lookup[i].com_call)(command);
+			(*lookup[i].com_call)(option);
 		}
 				
 	}
@@ -37,4 +41,19 @@ void call_command(char *command)
 void quit(char *dummy)
 {
 	e=1;
+}
+
+char *remove_spaces(char *ip)
+{
+	__uint32_t cnt=0;
+	char *op;	
+	for(__uint32_t i=0; ip[i]; i++)
+	{
+		if(ip[i]!=' ' && ip[i]!='\n')
+		{
+			op[cnt++]=ip[i];
+		}
+	}
+	op[cnt]='\0';
+	return op;
 }
--- main.h
+++ main.h
@@ -5,7 +5,9 @@
 
 typedef struct { char *com; void (*com_call)(char *);} __attribute__((packed))decode;
 
-void call_command(char *command);
+void call_command(char *command, char *option);
 
 void quit(char *dummy);
+
+char *remove_spaces(char *ip);
 
