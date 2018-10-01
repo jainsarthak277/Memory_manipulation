cc62ae0377597cdf05624760972f77fbacb19003
VatsalSheth <vash3665@colorado.edu>
2018-10-01 00:09:38 -0600

    Write bug fixed

--- write.c
+++ write.c
@@ -3,7 +3,7 @@
 void write_fn(char *addr, char *val)
 {
 	tmp = (__uint32_t *)malloc(4);
-	if(addr!=NULL && val!=NULL)
+	if(addr!=NULL && val!=NULL && mem_status==1)
 	{
 		if(strncmp(addr,"addr",4)==0)
 		{
@@ -39,6 +39,14 @@ void write_fn(char *addr, char *val)
 		{
 			printf("Invalid option for write !!!\n");
 		}
+	}
+	else if(strcmp(addr,"help")==0)
+	{
+		help_fn("write",val);
+	}
+	else if(mem_status==0)
+	{
+		printf("First allocate memory...\n");
 	}
 	else
 	{
