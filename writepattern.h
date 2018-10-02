7d5d8f5eec2dd08b7ea87f2a265858304d6978d2
VatsalSheth <vash3665@colorado.edu>
2018-10-02 02:54:42 -0600

    Pattern bugs fixed

--- writepattern.h
+++ writepattern.h
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include "allocate.h"
 #include "prng.h"
 
 void write_pattern_fn(char* addr_wp, char* siz, char* sd);
 
 long address_wp, addr_diff_wp;
-__uint32_t* tmp_wp, size, seed;
+__uint32_t* tmp_wp, size, seed, valid_op, err, off;
